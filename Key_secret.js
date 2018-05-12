9593d9aa50b42b2b8875150472e173540fd05914
3d4K9ZlAUUic <33258239+SevenGoldenHorses@users.noreply.github.com>
2018-05-12 18:29:00 +0200

    token cambio

--- Key_secret.js
+++ Key_secret.js
@@ -1,8 +1,8 @@
 module.exports = {
   consumer_key:         'QGvlMuQf42gOtTnrQs3Drg56p',
   consumer_secret:      'aTN3TAQCL0DxXYUX4kkAtPALnqXBPjWSmTwwTcMo79Du9PxQ83',
-  access_token:         '936671327737536512-XVFYfhrNOAl38zvh7O1PektA8EwIl1M',
-  access_token_secret:  'haRoboyIJZ2HK88W9Q6pGH8DFbslzsnghIKTs8J5LQbmz',
+  access_token:         '936671327737536512-KdBZZFt3DRdC0ho0q3XavLWK3CeQ1xr',
+  access_token_secret:  'vXRDRDVw4XChpVVzw1tVIvBofjXLR4qd2x3kM4grkv021',
   // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
 
